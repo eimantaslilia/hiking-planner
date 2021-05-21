3bb4267fc0229862d80599d4d986c0e99e3c3d77
unknown <eimantas.lilia@gmail.com>
2021-05-21 21:43:38 +0300

    HikingPlanner: Initial spring boot config

---
+++ src/test/java/com/outdoors/hiking/HikingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outdoors.hiking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
